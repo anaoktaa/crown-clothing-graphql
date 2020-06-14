5cb1bd730d2e9beac2d053a78ea4b13e9f142150
@anaokta <ana.oktaviana.ana@gmail.com>
2020-06-14 16:09:24 +0700

    implementing graphql in collection.component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
@@ -23,24 +23,6 @@ const client = new ApolloClient({
   link: httpLink,
   cache
 });
-
-client.query({
-  query: gql`
-    {
-      getCollectionsByTitle(title: "hats") {
-        id
-        title
-        items {
-          id
-          name
-          price
-          imageUrl
-        }
-      }
-    }
-  `
-}).then((res) => console.log(res))
-.catch((err) => console.log(err))
 
 ReactDOM.render(
   <ApolloProvider client={client}>
